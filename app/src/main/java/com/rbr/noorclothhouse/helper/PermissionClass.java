a89acd1c6288e6dbca03c3b8b71e6cc624223b98
Rahul Dhawan <rahuldhawan755@gmail.com>
2020-07-03 11:13:04 +0530

    Permissions added

---
+++ app/src/main/java/com/rbr/noorclothhouse/helper/PermissionClass.java
@@ -0,0 +1,24 @@
+package com.rbr.noorclothhouse.helper;
+
+import android.app.Activity;
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.ArrayList;
+
+public class PermissionClass extends AppCompatActivity {
+
+
+    public static void getPermissions(ArrayList<String> permission, Context c, Activity a) {
+
+        String[] permissionList = new String[permission.size()];
+        permission.toArray(permissionList);
+
+        ActivityCompat.requestPermissions(a, permissionList, 1111);
+
+
+    }
+
+}
