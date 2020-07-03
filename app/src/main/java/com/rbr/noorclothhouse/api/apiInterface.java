e5e43e5fec05a27801af0403c20c00866ad80f03
Rahul Dhawan <rahuldhawan755@gmail.com>
2020-07-03 23:06:35 +0530

    Add Stock Screen Completed with api

---
+++ app/src/main/java/com/rbr/noorclothhouse/api/apiInterface.java
@@ -0,0 +1,14 @@
+package com.rbr.noorclothhouse.api;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface apiInterface {
+
+    @FormUrlEncoded
+    @POST("stock_add.php")
+    Call<ResponseClass> addStockServer(@Field("image") String image, @Field("scode") String scode, @Field("sname") String name,
+                                @Field("sprice") String price, @Field("squantity") String quantity);
+}
